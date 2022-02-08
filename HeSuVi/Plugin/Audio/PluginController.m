eba9870c722c7c69b0d268bc83c3e6393b3e4272
Christopher Snowhill <kode54@gmail.com>
2022-02-07 23:54:46 -0800

    Excised RetroArch from this place

--- HeSuVi/Plugin/Audio/PluginController.m
+++ HeSuVi/Plugin/Audio/PluginController.m
@@ -16,169 +16,145 @@
 
 static PluginController *sharedPluginController = nil;
 
-+ (id<CogPluginController>)sharedPluginController
-{
++ (id<CogPluginController>)sharedPluginController {
 	@synchronized(self) {
-		if (sharedPluginController == nil) {
+		if(sharedPluginController == nil) {
 			sharedPluginController = [[self alloc] init];
 		}
 	}
-	
+
 	return sharedPluginController;
 }
 
-
 - (id)init {
 	self = [super init];
-	if (self) {
-        self.sources = [[NSMutableDictionary alloc] init];
- 
-        self.decodersByExtension = [[NSMutableDictionary alloc] init];
-        self.decodersByMimeType = [[NSMutableDictionary alloc] init];
-        
-        [self setup];
+	if(self) {
+		self.sources = [[NSMutableDictionary alloc] init];
+
+		self.decodersByExtension = [[NSMutableDictionary alloc] init];
+		self.decodersByMimeType = [[NSMutableDictionary alloc] init];
+
+		[self setup];
 	}
-	
+
 	return self;
 }
 
-- (void)setup
-{
-	if (self.configured == NO) {
+- (void)setup {
+	if(self.configured == NO) {
 		self.configured = YES;
-		
+
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(bundleDidLoad:) name:NSBundleDidLoadNotification object:nil];
 
 		[self loadPlugins];
 		[self printPluginInfo];
-	}	
+	}
 }
 
-- (void)bundleDidLoad:(NSNotification *)notification
-{
+- (void)bundleDidLoad:(NSNotification *)notification {
 	NSArray *classNames = [[notification userInfo] objectForKey:@"NSLoadedClasses"];
-	for (NSString *className in classNames)
-	{
+	for(NSString *className in classNames) {
 		Class bundleClass = NSClassFromString(className);
-		if ([bundleClass conformsToProtocol:@protocol(CogDecoder)]) {
+		if([bundleClass conformsToProtocol:@protocol(CogDecoder)]) {
 			[self setupDecoder:className];
 		}
-		if ([bundleClass conformsToProtocol:@protocol(CogSource)]) {
+		if([bundleClass conformsToProtocol:@protocol(CogSource)]) {
 			[self setupSource:className];
 		}
 	}
 }
 
-- (void)loadPluginsAtPath:(NSString *)path
-{
+- (void)loadPluginsAtPath:(NSString *)path {
 	NSArray *dirContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:nil];
 
-	for (NSString *pname in dirContents)
-	{
+	for(NSString *pname in dirContents) {
 		NSString *ppath;
-		ppath = [NSString pathWithComponents:@[path,pname]];
-		
-		if ([[pname pathExtension] isEqualToString:@"bundle"])
-		{
+		ppath = [NSString pathWithComponents:@[path, pname]];
+
+		if([[pname pathExtension] isEqualToString:@"bundle"]) {
 			NSBundle *b = [NSBundle bundleWithPath:ppath];
 			[b load];
 		}
 	}
 }
 
-- (void)loadPlugins
-{
-    NSString * path = [[NSBundle mainBundle] builtInPlugInsPath];
-    [self loadPluginsAtPath:path];
+- (void)loadPlugins {
+	NSString *path = [[NSBundle mainBundle] builtInPlugInsPath];
+	[self loadPluginsAtPath:path];
 }
 
-- (void)setupDecoder:(NSString *)className
-{
+- (void)setupDecoder:(NSString *)className {
 	Class decoder = NSClassFromString(className);
-	if (decoder && [decoder respondsToSelector:@selector(fileTypes)]) {
-		for (id fileType in [decoder fileTypes])
-		{
-            NSString *ext = [fileType lowercaseString];
-            NSMutableArray *decoders;
-            if (![decodersByExtension objectForKey:ext])
-            {
-                decoders = [[NSMutableArray alloc] init];
-                [decodersByExtension setObject:decoders forKey:ext];
-            }
-            else
-                decoders = [decodersByExtension objectForKey:ext];
+	if(decoder && [decoder respondsToSelector:@selector(fileTypes)]) {
+		for(id fileType in [decoder fileTypes]) {
+			NSString *ext = [fileType lowercaseString];
+			NSMutableArray *decoders;
+			if(![decodersByExtension objectForKey:ext]) {
+				decoders = [[NSMutableArray alloc] init];
+				[decodersByExtension setObject:decoders forKey:ext];
+			} else
+				decoders = [decodersByExtension objectForKey:ext];
 			[decoders addObject:className];
 		}
 	}
-	
-	if (decoder && [decoder respondsToSelector:@selector(mimeTypes)]) {
-		for (id mimeType in [decoder mimeTypes]) 
-		{
-            NSString *mimetype = [mimeType lowercaseString];
-            NSMutableArray *decoders;
-            if (![decodersByMimeType objectForKey:mimetype])
-            {
-                decoders = [[NSMutableArray alloc] init];
-                [decodersByMimeType setObject:decoders forKey:mimetype];
-            }
-            else
-                decoders = [decodersByMimeType objectForKey:mimetype];
-            [decoders addObject:className];
+
+	if(decoder && [decoder respondsToSelector:@selector(mimeTypes)]) {
+		for(id mimeType in [decoder mimeTypes]) {
+			NSString *mimetype = [mimeType lowercaseString];
+			NSMutableArray *decoders;
+			if(![decodersByMimeType objectForKey:mimetype]) {
+				decoders = [[NSMutableArray alloc] init];
+				[decodersByMimeType setObject:decoders forKey:mimetype];
+			} else
+				decoders = [decodersByMimeType objectForKey:mimetype];
+			[decoders addObject:className];
 		}
 	}
 }
 
-- (void)setupSource:(NSString *)className
-{
+- (void)setupSource:(NSString *)className {
 	Class source = NSClassFromString(className);
-	if (source && [source respondsToSelector:@selector(schemes)]) {
-		for (id scheme in [source schemes])
-		{
+	if(source && [source respondsToSelector:@selector(schemes)]) {
+		for(id scheme in [source schemes]) {
 			[sources setObject:className forKey:scheme];
 		}
 	}
 }
 
-- (void)printPluginInfo
-{
+- (void)printPluginInfo {
 	ALog(@"Sources: %@", self.sources);
 
 	ALog(@"Decoders by Extension: %@", self.decodersByExtension);
 	ALog(@"Decoders by Mime Type: %@", self.decodersByMimeType);
 }
 
-- (id<CogSource>) audioSourceForURL:(NSURL *)url
-{
+- (id<CogSource>)audioSourceForURL:(NSURL *)url {
 	NSString *scheme = [url scheme];
-	
+
 	Class source = NSClassFromString([sources objectForKey:scheme]);
-    
+
 	return [[source alloc] init];
 }
 
-//Note: Source is assumed to already be opened.
-- (id<CogDecoder>) audioDecoderForSource:(id <CogSource>)source skipCue:(BOOL)skip
-{
+// Note: Source is assumed to already be opened.
+- (id<CogDecoder>)audioDecoderForSource:(id<CogSource>)source skipCue:(BOOL)skip {
 	NSString *ext = [[source url] pathExtension];
 	NSArray *decoders = [decodersByExtension objectForKey:[ext lowercaseString]];
-    NSString *classString;
-    if (decoders) {
-        classString = [decoders objectAtIndex:0];
-    }
-	else {
-        decoders = [decodersByMimeType objectForKey:[[source mimeType] lowercaseString]];
-        if (decoders) {
-            classString = [decoders objectAtIndex:0];
-        }
-        else {
-            classString = @"SilenceDecoder";
-        }
+	NSString *classString;
+	if(decoders) {
+		classString = [decoders objectAtIndex:0];
+	} else {
+		decoders = [decodersByMimeType objectForKey:[[source mimeType] lowercaseString]];
+		if(decoders) {
+			classString = [decoders objectAtIndex:0];
+		} else {
+			classString = @"SilenceDecoder";
+		}
 	}
 
 	Class decoder = NSClassFromString(classString);
-	
+
 	return [[decoder alloc] init];
 }
 
 @end
-
